be135967d7ebea27683b423ac1770646683b9d04
Hillary Kane <hillaryrkane47@gmail.com>
2017-01-09 13:25:54 -0500

    legend that is wack a doodle

--- script.js
+++ script.js
@@ -1,51 +1,83 @@
-
 console.log('script loaded')
-
+// skully x=860 y=238
+// mulde
 function InitChart() {
 
-var all_section = [{
- "line": "mulder", unit:1, fun:7.4}, {"line": "mulder", unit:2, fun:6.7}, {"line": "mulder", unit:3, fun:5}, {"line": "mulder", unit:4, fun:4.1}, {"line": "skully", unit:1, fun:7.5}, {"line": "skully", unit:2, fun:7.3}, {"line": "skully", unit:3, fun:6}, {"line": "skully", unit:4, fun:5.5}, {"line":"mufasa", unit:1, fun:7.7}, {"line":"mufasa", unit:2, fun:7.3}, {"line":"mufasa", unit:3, fun:5.7}, {"line":"mufasa", unit:4, fun:4.9}, {"line":"scar", unit:1, fun:6.8}, {"line":"scar", unit:2, fun:6.3}, {"line":"scar", unit:3, fun:5.5}, {"line":"scar", unit:4, fun:5.3}, {"line":"tenDays", unit:1, fun:7.9}, {"line":"tenDays", unit:2, fun:7.8}, {"line":"tenDays", unit:3, fun:6.4}, {"line":"tenDays", unit:4, fun:5.3}, {"line":"wedPlan", unit:1, fun:6.9}, {"line":"wedPlan", unit:2, fun:6.1}, {"line":"wedPlan", unit:3, fun:4.6}, {"line":"wedPlan", unit:4, fun:4.6}, {"line":"nsync", unit:1, fun:7.6}, {"line":"nsync", unit:2, fun:7.6}, {"line":"nsync", unit:3, fun:5.6}, {"line":"nsync", unit:4, fun:4.4}, {"line":"bsb", unit:1, fun:7}, {"line":"bsb", unit:2, fun:5.6}, {"line":"bsb", unit:3, fun:5.6}, {"line":"bsb", unit:4, fun:6.8}, {"line":"leo", unit:1, fun:7.5}, {"line":"leo", unit:2, fun:7.1}, {"line":"leo",  unit:3, fun:5.6}, {"line":"leo",  unit:4, fun:5}];
-
-var allGroup = d3.nest()
-    .key(function(d) {return d.line})
-    .entries(all_section);
-
 var data_wdi = [{
- "line": "mulder", unit:1, fun:7.4}, {"line": "mulder", unit:2, fun:6.7}, {"line": "mulder", unit:3, fun:5}, {"line": "mulder", unit:4, fun:4.1}, {"line": "skully", unit:1, fun:7.5}, {"line": "skully", unit:2, fun:7.3}, {"line": "skully", unit:3, fun:6}, {"line": "skully", unit:4, fun:5.5}];
+    "line": "mulder", "unit":1, "fun":7.4},
+    {"line": "mulder", "unit":2, "fun":6.7},
+    {"line": "mulder", "unit":3, "fun":5},
+    {"line": "mulder", "unit":4, "fun":4.1},
+    {"line": "skully", "unit":1, "fun":7.5},
+    {"line": "skully", "unit":2, "fun":7.3},
+    {"line": "skully", "unit":3, "fun":6},
+    {"line": "skully", "unit":4, "fun":5.5}
+    ];
 
  var wdiGroup = d3.nest()
-    .key(function(d) {return d.line;})
+    .key(function(d) { return d.line; })
     .entries(data_wdi);
 
-    var data_lion = [{
-  "line":"mufasa", unit:1, fun:7.7}, {"line":"mufasa", unit:2, fun:7.3}, {"line":"mufasa", unit:3, fun:5.7}, {"line":"mufasa", unit:4, fun:4.9}, {"line":"scar", unit:1, fun:6.8}, {"line":"scar", unit:2, fun:6.3}, {"line":"scar", unit:3, fun:5.5}, {"line":"scar", unit:4, fun:5.3}];
+var data_lion = [
+    {"line":"mufasa", "unit":1, "fun":7.7},
+    {"line":"mufasa", "unit":2, "fun":7.3},
+    {"line":"mufasa", "unit":3, "fun":5.7},
+    {"line":"mufasa", "unit":4, "fun":4.9},
+    {"line":"scar", "unit":1, "fun":6.8},
+    {"line":"scar", "unit":2, "fun":6.3},
+    {"line":"scar", "unit":3, "fun":5.5},
+    {"line":"scar", "unit":4, "fun":5.3}
+  ];
 
 var lionGroup = d3.nest()
     .key(function(d) {return d.line;})
     .entries(data_lion);
 
-var data_matt=[{"line":"tenDays", unit:1, fun:7.9}, {"line":"tenDays", unit:2, fun:7.8}, {"line":"tenDays", unit:3, fun:6.4}, {"line":"tenDays", unit:4, fun:5.3}, {"line":"wedPlan", unit:1, fun:6.9}, {"line":"wedPlan", unit:2, fun:6.1}, {"line":"wedPlan", unit:3, fun:4.6}, {"line":"wedPlan", unit:4, fun:4.6}];
+var data_matt=[
+    {"line":"tenDays", "unit":1, "fun":7.9},
+    {"line":"tenDays", "unit":2, "fun":7.8},
+    {"line":"tenDays", "unit":3, "fun":6.4},
+    {"line":"tenDays", "unit":4, "fun":5.3},
+    {"line":"wedPlan", "unit":1, "fun":6.9},
+    {"line":"wedPlan", "unit":2, "fun":6.1},
+    {"line":"wedPlan", "unit":3, "fun":4.6},
+    {"line":"wedPlan", "unit":4, "fun":4.6}
+  ];
 
 var mattGroup = d3.nest()
     .key(function(d) {return d.line;})
     .entries(data_matt);
 
-var data_band=[{"line":"nsync", unit:1, fun:7.6}, {"line":"nsync", unit:2, fun:7.6}, {"line":"nsync", unit:3, fun:5.6}, {"line":"nsync", unit:4, fun:4.4}, {"line":"bsb", unit:1, fun:7}, {"line":"bsb", unit:2, fun:5.6}, {"line":"bsb", unit:3, fun:5.6}, {"line":"bsb", unit:4, fun:6.8}]
+var data_band=[
+    {"line":"nsync", "unit":1, "fun":7.6},
+    {"line":"nsync", "unit":2, "fun":7.6},
+    {"line":"nsync", "unit":3, "fun":5.6},
+    {"line":"nsync", "unit":4, "fun":4.4},
+    {"line":"bsb", "unit":1, "fun":7},
+    {"line":"bsb", "unit":2, "fun":5.6},
+    {"line":"bsb", "unit":3, "fun":5.6},
+    {"line":"bsb", "unit":4, "fun":6.8}
+  ];
 
 var bandGroup = d3.nest()
     .key(function(d) {return d.line;})
     .entries(data_band);
 
-var data_leo=[{"line":"leo", unit:1, fun:7.5}, {"line":"leo", unit:2, fun:7.1}, {"line":"leo", unit:3, fun:5.6}, {"line":"leo", unit:4, fun:5}]
+var data_leo=[
+    {"line":"leo", "unit":1, "fun":7.5},
+    {"line":"leo", "unit":2, "fun":7.1},
+    {"line":"leo",  "unit":3, "fun":5.6},
+    {"line":"leo",  "unit":4, "fun":5}
+];
 
 var leoGroup = d3.nest()
     .key(function(d) {return d.line;})
     .entries(data_leo);
 
- // console.log(JSON.stringify(sectionGroup[0]));
+
 //setting SVG skeleton
 var sec = d3.select("#section"),
-    WIDTH = 900,
+    WIDTH = 860,
     HEIGHT = 500,
     MARGINS = {
         top: 30,
@@ -53,9 +85,6 @@ var sec = d3.select("#section"),
         bottom: 30,
         left: 20
   };
-
-//defining space below x axis based on data.length in order to make legends display
-// lSpace = WIDTH/sectionGroup.length;
 
 //scaling x & y axis to match Fun 1-10 scale y-axis and Units 1-4 x-axis
   xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([1, 4])
@@ -116,46 +145,30 @@ var sec = d3.select("#section"),
       })
       .interpolate("basis");
 
-// for graphing all lines at once - default
-// allGroup.forEach(function(d, i) {
-//   sec.append('svg:path')
-//       .attr('d', lineGen(d.values))
-//       .attr('stroke', function(d, j) {
-//          return "hsl(" + Math.random() * 360 + ",100%,50%)";
-//         })
-//         .attr('stroke-width', 2)
-//         .attr('id', 'line_' + d.key)
-//         .attr('fill', 'none');
-// });
-
 function updateWDI() {
-
   sec.selectAll('.line').remove();
+    wdiGroup.forEach(function(d, i) {
+    sec.append('svg:path')
+        .attr('d', lineGen(d.values))
+        .attr('stroke', function(d, j) {
+           return "hsl(" + Math.random() * 360 + ",100%,50%)";
+          })
+          .attr('stroke-width', 2)
+          .attr('id', 'line_' + d.key)
+          .attr('fill', 'none')
+          .attr("class", "line")
+    });
 
-  wdiGroup.forEach(function(d, i) {
-  sec.append('svg:path')
-      .attr('d', lineGen(d.values))
-      .attr('stroke', function(d, j) {
-         return "hsl(" + Math.random() * 360 + ",100%,50%)";
-        })
-        .attr('stroke-width', 2)
-        .attr('id', 'line_' + d.key)
-        .attr('fill', 'none')
-        .attr("class", "line")
-
-     sec.append("text")
-        .attr("x", 900)
-        .attr("y", HEIGHT)
-        .style("fill", "black")
-        .attr("class", "legend")
-        .attr("text-anchor", "start")
-        .text(d.key);
-  });
+  document.getElementById('wdi-legend').style.display = '';
+  document.getElementById('lion-legend').style.display = 'none';
+  document.getElementById('matt-legend').style.display = 'none';
+  document.getElementById('boyBand-legend').style.display = 'none';
 }
 
 function updateLion() {
 
   sec.selectAll('.line').remove();
+  sec.selectAll('.legend').remove();
 
   lionGroup.forEach(function(d, i) {
     sec.append('svg:path')
@@ -168,19 +181,18 @@ function updateLion() {
       .attr('fill', 'none')
       .attr("class","line");
 
-    sec.append("text")
-      .attr("x", 900) // .attr("x", (lSpace/2)+i*lSpace)
-      .attr("y", HEIGHT) // .attr("y", HEIGHT)
-      .style("fill", "black")
-      .attr("class","legend")
-      .attr("text-anchor", "start")
-      .text(d.key);
   });
+
+  document.getElementById('wdi-legend').style.display = 'none';
+  document.getElementById('lion-legend').style.display = '';
+  document.getElementById('matt-legend').style.display = 'none';
+  document.getElementById('boyBand-legend').style.display = 'none';
 }
 
 function updateMatt() {
 
   sec.selectAll('.line').remove();
+  sec.selectAll('.legend').remove();
 
   mattGroup.forEach(function(d, i) {
     sec.append('svg:path')
@@ -193,19 +205,18 @@ function updateMatt() {
       .attr('fill', 'none')
       .attr("class","line");
 
-    sec.append("text")
-      .attr("x", 900) // .attr("x", (lSpace/2)+i*lSpace)
-      .attr("y", HEIGHT) // .attr("y", HEIGHT)
-      .style("fill", "black")
-      .attr("class","legend")
-      .attr("text-anchor", "start")
-      .text(d.key);
   });
+
+  document.getElementById('wdi-legend').style.display = 'none';
+  document.getElementById('lion-legend').style.display = 'none';
+  document.getElementById('matt-legend').style.display = '';
+  document.getElementById('boyBand-legend').style.display = 'none';
 }
 
 function updateBand() {
 
   sec.selectAll('.line').remove();
+  sec.selectAll('.legend').remove();
 
   bandGroup.forEach(function(d, i) {
     sec.append('svg:path')
@@ -218,19 +229,18 @@ function updateBand() {
       .attr('fill', 'none')
       .attr("class","line");
 
-    sec.append("text")
-      .attr("x", 900) // .attr("x", (lSpace/2)+i*lSpace)
-      .attr("y", HEIGHT) // .attr("y", HEIGHT)
-      .style("fill", "black")
-      .attr("class","legend")
-      .attr("text-anchor", "start")
-      .text(d.key);
   });
+
+  document.getElementById('wdi-legend').style.display = 'none';
+  document.getElementById('lion-legend').style.display = 'none';
+  document.getElementById('matt-legend').style.display = 'none';
+  document.getElementById('boyBand-legend').style.display = '';
 }
 
 function updateLeo() {
 
   sec.selectAll('.line').remove();
+  sec.selectAll('.legend').remove();
 
   leoGroup.forEach(function(d, i) {
     sec.append('svg:path')
@@ -243,14 +253,13 @@ function updateLeo() {
       .attr('fill', 'none')
       .attr("class","line");
 
-     sec.append("text")
-      .attr("x", 900) // .attr("x", (lSpace/2)+i*lSpace)
-      .attr("y", HEIGHT) // .attr("y", HEIGHT)
-      .style("fill", "black")
-      .attr("class","legend")
-      .attr("text-anchor", "start")
-      .text(d.key);
+
   });
+
+  document.getElementById('wdi-legend').style.display = 'none';
+  document.getElementById('lion-legend').style.display = 'none';
+  document.getElementById('matt-legend').style.display = 'none';
+  document.getElementById('boyBand-legend').style.display = 'none';
 }
 
 
@@ -266,6 +275,11 @@ function updateLeo() {
   matt.addEventListener("click", updateMatt, false);
   band.addEventListener("click", updateBand, false);
   leo.addEventListener("click", updateLeo, false);
+
+  document.getElementById('wdi-legend').style.display = 'none';
+  document.getElementById('lion-legend').style.display = 'none';
+  document.getElementById('matt-legend').style.display = 'none';
+  document.getElementById('boyBand-legend').style.display = 'none';
 
 } //end of InitChart function
 
